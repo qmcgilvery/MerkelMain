51dd44f410cd3556985bf9c50a7f188fed191a76
Quentin McGilvery <qmcgilvery@gmail.com>
2022-04-08 19:30:21 -0700

    Initial Commit

---
+++ Shared/Object_Oriented_ProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Object_Oriented_ProgrammingApp.swift
+//  Shared
+//
+//  Created by quentin mcgilvery on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Object_Oriented_ProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
